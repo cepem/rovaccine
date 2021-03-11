8a236c1b0f549ff7d4b844363302fcaad24c927d
Emanuel Cepoi <emanuel.cepoi@outlook.com>
2021-03-11 14:55:27 +0200

    Initial commit

--- README.md
+++ README.md
@@ -1,2 +1,18 @@
-# rovaccine
-Node.js script that notifies you whenever there's a COVID Vaccine slot available in your area
+#rovaccine
+[![](https://i.imgur.com/CpIMJBM.png)](https://i.imgur.com/CpIMJBM.pnghttp://)
+
+Simple Node.js cron that checks the Official Romanian Covid Vaccine platform for available slots in your area.
+
+**Usage:**
+
+1. Clone this repo
+2. `npm install`
+3. Change the PROFILE_DATA object with your data.
+4. `node app.js`
+
+The script will make a request every 30 seconds and it'll notify you once it finds availble slot
+
+## FAQ
+**Where do I get the data for the PROFILE_DATA object ? **
+This script is already extremly easy to setup and use therefore I'll let you figure that one on your own.
+
---
+++ app.js
@@ -0,0 +1,68 @@
+const cron = require("node-cron");
+const fetch = require("node-fetch");
+const notifier = require("node-notifier");
+
+const PROFILE_DATA = {
+  cookie: "",
+  countyID: "",
+  localityID: "",
+  identificationCode: "",
+  masterPersonnelCategoryID: -4,
+  personnelCategoryID: 32,
+  recipientID: "",
+};
+
+const getVaccineSlotsData = () => {
+  
+  return new Promise((resolve, reject) => {
+    fetch(
+      "https://programare.vaccinare-covid.gov.ro/scheduling/api/centres?page=0&size=10&sort=,",
+      {
+        headers: {
+          accept: "application/json, text/plain, */*",
+          "accept-language": "en-US,en;q=0.9",
+          "content-type": "application/json",
+          "sec-ch-ua":
+            '"Google Chrome";v="87", " Not;A Brand";v="99", "Chromium";v="87"',
+          "sec-ch-ua-mobile": "?0",
+          "sec-fetch-dest": "empty",
+          "sec-fetch-mode": "cors",
+          "sec-fetch-site": "same-origin",
+          cookie: PROFILE_DATA.cookie,
+        },
+        referrer: "https://programare.vaccinare-covid.gov.ro/",
+        referrerPolicy: "strict-origin-when-cross-origin",
+        body: `{"countyID":${PROFILE_DATA.countyID},"localityID":${PROFILE_DATA.localityID},"name":null,"identificationCode":${PROFILE_DATA.identificationCode},"masterPersonnelCategoryID":${PROFILE_DATA.masterPersonnelCategoryID},"personnelCategoryID":${PROFILE_DATA.personnelCategoryID},"recipientID":${PROFILE_DATA.recipientID}}`,
+        method: "POST",
+        mode: "cors",
+      }
+    )
+      .then((r) => resolve(r))
+      .catch((err) => reject(err));
+  });
+};
+
+const parseVaccineResponse = (vaccineResponse) => {
+  const availableLocations = vaccineResponse.filter(
+    (item, index) => item.availableSlots > 0
+  );
+
+  if (availableLocations.length > 0) {
+    availableLocations.forEach((item, index) => {
+      notifier.notify({
+        title: "Sloturi de vaccine disponibile!",
+        message: `${item.availableSlots} Sloturi la: ${item.name} | ${item.address} `,
+      });
+    });
+  }
+
+  if (availableLocations.length === 0) {
+    console.log("N-au fost găsite slot-uri");
+  }
+};
+
+cron.schedule("*/30 * * * * *", () => {
+  getVaccineSlotsData()
+    .then((r) => r.text())
+    .then((r2) => parseVaccineResponse(JSON.parse(r2).content));
+});
